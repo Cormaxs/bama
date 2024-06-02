3376876fa5f278e0742c1b7e92a43f522299392d
cormaxs <tomasbarros076@gmail.com>
2024-06-02 04:41:59 +0200

    usando variabl global

---
+++ secciones/artistas/carolinaIbarra/links.js
@@ -0,0 +1,9 @@
+window.links = [
+  "https://i.ibb.co/ZXnDrjj/image.jpg",
+  "https://i.ibb.co/C79gpM3/1.jpg",
+  "https://i.ibb.co/SxPmVJw/2.jpg",
+  "https://i.ibb.co/DQGg65T/3.jpg",
+  "https://i.ibb.co/mv5Zp39/4.jpg",
+    ];
+ // console.log(links);
+
---
+++ secciones/artistas/load/links.js
@@ -0,0 +1,19 @@
+window.links = [
+  "https://i.ibb.co/D1CW9vW/image.jpg",
+  "https://i.ibb.co/vzRkwmy/1.jpg",
+  "https://i.ibb.co/GTvCZnK/2.jpg",
+  "https://i.ibb.co/bsjkXSf/3.jpg",
+  "https://i.ibb.co/Ht15dys/4.jpg",
+  "https://i.ibb.co/rpKRwsg/5.jpg",
+  "https://i.ibb.co/8drRVqr/6.jpg",
+  "https://i.ibb.co/F463gW2/7.jpg",
+  "https://i.ibb.co/pQV13Wv/8.jpg",
+  "https://i.ibb.co/wZHXrZm/9.jpg",
+  "https://i.ibb.co/Lxwdj1q/10.jpg",
+  "https://i.ibb.co/4ZG1CFh/11.jpg",
+  "https://i.ibb.co/P171Qh9/12.jpg",
+  "https://i.ibb.co/PCLZn3n/13.jpg",
+  "https://i.ibb.co/d6rHz7r/14.jpg",
+  ];
+ 
+
