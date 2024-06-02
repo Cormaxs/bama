e1165e0d8db964f23d7e94ac542c312ac965d31a
cormaxs <tomasbarros076@gmail.com>
2024-06-02 03:31:07 +0200

    nueva estructuracion

--- js/galeria.js
+++ js/galeria.js
@@ -1,18 +1,5 @@
 // Ejemplo de array con links de imágenes (al menos 5)
-const imagenes = [
-  "https://i.ibb.co/s5vg7Jq/image.jpg",
-"https://i.ibb.co/8cC0LtP/1.jpg",
-"https://i.ibb.co/0Q31XtJ/2.jpg",
-"https://i.ibb.co/M6V0kGr/3.jpg",
-"https://i.ibb.co/WyvRkPV/4.jpg",
-"https://i.ibb.co/n7nZvf7/5.jpg",
-"https://i.ibb.co/mCrhB8t/6.jpg",
-"https://i.ibb.co/6t3N0GM/7.jpg",
-"https://i.ibb.co/3Fzsp38/8.jpg",
-"https://i.ibb.co/J79Frb2/9.jpg",
-"https://i.ibb.co/vL6gy3G/10.jpg",
-"https://i.ibb.co/wWCBWSr/11.jpg"
-  ];
+import imagenes from './links.js';
   
   // Función para generar la estructura HTML
   function generarEstructuraHTML(imagenes) {
