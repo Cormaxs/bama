34aec031378fd36f97e3ee1244813cbf66b1f82f
cormaxs <tomasbarros076@gmail.com>
2024-06-12 22:00:04 +0200

    probando nuevo fomato

--- secciones/artistas/load/links.js
+++ secciones/artistas/load/links.js
@@ -1,18 +1,10 @@
 window.links = [
-  "https://i.ibb.co/D1CW9vW/image.jpg",
-  "https://i.ibb.co/PCLZn3n/13.jpg",
-  "https://i.ibb.co/GTvCZnK/2.jpg",
-  "https://i.ibb.co/bsjkXSf/3.jpg",
-  "https://i.ibb.co/Ht15dys/4.jpg",
-  "https://i.ibb.co/rpKRwsg/5.jpg",
-  "https://i.ibb.co/8drRVqr/6.jpg",
-  "https://i.ibb.co/F463gW2/7.jpg",
-  "https://i.ibb.co/pQV13Wv/8.jpg",
-  "https://i.ibb.co/wZHXrZm/9.jpg",
-  "https://i.ibb.co/Lxwdj1q/10.jpg",
-  "https://i.ibb.co/4ZG1CFh/11.jpg",
-  "https://i.ibb.co/P171Qh9/12.jpg",
-  "https://i.ibb.co/vzRkwmy/1.jpg",
-  "https://i.ibb.co/d6rHz7r/14.jpg"
-  ];
- 
+  "https://i.ibb.co/9WKjvbj/Bama-Fotografia-08.webp",
+  "https://i.ibb.co/DVLz5qW/Bama-Fotografia-12.webp",
+  "https://i.ibb.co/j5D7n6T/Bama-Fotografia-15.webp",
+  "https://i.ibb.co/N6x2M7v/Bama-Fotografia-18.webp",
+  "https://i.ibb.co/26k6NH7/Bama-Fotografia-21.webp",
+  "https://i.ibb.co/X7qZSrP/Bama-Fotografia-09.webp",
+  "https://i.ibb.co/bWVTHvS/Bama-Fotografia-11.webp",
+  "https://i.ibb.co/bQqBKZy/Bama-Fotografia-19.webp"
+];
