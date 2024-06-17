d3bc6ef74e7d49f652df80f7e39a7f9b6422b6e1
cormaxs <tomasbarros076@gmail.com>
2024-06-17 17:10:17 +0200

    nuevas letras

---
+++ secciones/productosStock/links.js
@@ -0,0 +1,16 @@
+window.links = [
+  "https://i.ibb.co/Sy4t2M1/Bama-Fotografia-206.webp",
+  "https://i.ibb.co/8bS4bB2/Bama-Fotografia-204.webp",
+  "https://i.ibb.co/DwxZzp8/Bama-Fotografia-59.webp",
+  "https://i.ibb.co/VWZ99Tf/Bama-Fotografia-60.webp",
+  "https://i.ibb.co/gFcqbgz/Bama-Fotografia-58.webp",
+  "https://i.ibb.co/bHJKDtq/Bama-Fotografia-57.webp",
+  "https://i.ibb.co/vJBGCwD/IMG-0334-Mejorado-NR.webp",
+  "https://i.ibb.co/XXgCS3j/IMG-0347-Mejorado-NR.webp",
+  "https://i.ibb.co/P46wVPN/IMG-0231-Mejorado-NR.webp",
+  "https://i.ibb.co/CmsxTPB/IMG-0332-Mejorado-NR.webp",
+  "https://i.ibb.co/pXgv1W1/IMG-0257.webp",
+  "https://i.ibb.co/3WcPjV3/IMG-0293-Mejorado-NR.webp",
+  "https://i.ibb.co/F7HppYq/IMG-0300-Mejorado-NR.webp",
+  "https://i.ibb.co/JKNR8Mk/IMG-0325.webp"
+];
