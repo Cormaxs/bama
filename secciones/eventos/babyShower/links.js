2bba9148fb3abeb34066c2a744baf230c6ef86a5
cormaxs <tomasbarros076@gmail.com>
2024-06-22 16:59:55 +0200

    modificaciones y agregue carpetas

---
+++ secciones/eventos/babyShower/links.js
@@ -0,0 +1,17 @@
+window.links = [ 
+  "https://i.ibb.co/Tvt1Nyj/bama-fotografia-298.webp",
+   "https://i.ibb.co/SmHTNFJ/bama-fotografia-294.webp",
+   "https://i.ibb.co/VWWxH1r/bama-fotografia-287.webp",
+    "https://i.ibb.co/MCVBWsk/bama-fotografia-362.webp",
+  "https://i.ibb.co/D9Cd1M2/bama-fotografia-306.webp",
+
+  "https://i.ibb.co/jLr377V/bama-fotografia-209.webp",
+  "https://i.ibb.co/VBYrzhx/bama-fotografia-276-1.webp",
+  "https://i.ibb.co/zJ7jvgk/bama-fotografia-203-1.webp",
+  
+  "https://i.ibb.co/WPwjjvd/bama-fotografia-221-1.webp",
+  "https://i.ibb.co/vBhz2jS/bama-fotografia-236-1.webp",
+  
+    "https://i.ibb.co/NZG4ZH4/bama-fotografia-245.webp"
+
+];
---
+++ secciones/redesSociales/claseYogaEnergym/links.js
@@ -0,0 +1,12 @@
+window.links = [
+  "https://i.ibb.co/Bw8k99j/image-3.webp",
+  "https://i.ibb.co/WsT44t8/image-7.webp",
+  "https://i.ibb.co/tBYS3XZ/image-3-1.webp",
+  "https://i.ibb.co/qWb02yZ/image-6.webp",
+  "https://i.ibb.co/xYHLQMW/image-1-3.webp",
+  "https://i.ibb.co/QF3Rkvs/image-5.webp",
+  "https://i.ibb.co/khrDM1K/image-1-2.webp",
+  "https://i.ibb.co/Ch49dsZ/image-2-2.webp",
+  "https://i.ibb.co/sWL76PV/image-4.webp"
+
+];
