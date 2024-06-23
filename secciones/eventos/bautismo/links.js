347554b0c67e3854b3c0d141390875fbfff7e8f4
cormaxs <tomasbarros076@gmail.com>
2024-06-24 00:23:09 +0200

    agregue bautismo

---
+++ secciones/eventos/bautismo/links.js
@@ -0,0 +1,13 @@
+window.links = [
+
+  "https://i.ibb.co/6R2HWJG/Bama-Fotografia-9.webp",
+  "https://i.ibb.co/WyBrdbr/Bama-Fotografia-13.webp",
+  "https://i.ibb.co/Vt7tmFc/Bama-Fotografia-12.webp",
+  "https://i.ibb.co/s6nd8Jv/Bama-Fotografia-11.webp",
+  "https://i.ibb.co/6gkwVcT/Bama-Fotografia-10.webp",
+  "https://i.ibb.co/DGkcG9R/Bama-Fotografia-8.webp",
+  "https://i.ibb.co/ZmM6jGd/Bama-Fotografia-6.webp",
+  "https://i.ibb.co/RbSh3fz/Bama-Fotografia-14.webp",
+  "https://i.ibb.co/yyVvRQ4/Bama-Fotografia-7.webp"
+
+];
