b70a4db679f365953ac2685093abf9b245283213
cormaxs <tomasbarros076@gmail.com>
2024-06-23 15:24:46 +0200

    nueva carpeta talleres

---
+++ secciones/talleres/retrato15s/links.js
@@ -0,0 +1,10 @@
+window.links = [
+  "https://i.ibb.co/hZZLLTs/Bama-Fotografia-283.webp",
+  "https://i.ibb.co/2gKnyny/Bama-Fotografia-278.webp",
+  "https://i.ibb.co/WpD5xFF/Bama-Fotografia-281.webp",
+
+  "https://i.ibb.co/ngRS7Bm/Bama-Fotografia-279.webp",
+
+  "https://i.ibb.co/fGDRTkR/Bama-Fotografia-280.webp",
+  "https://i.ibb.co/GvsGDSt/Bama-Fotografia-282.webp"
+];
